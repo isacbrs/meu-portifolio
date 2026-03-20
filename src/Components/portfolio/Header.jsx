628b28f091673b79f44794376deed61021336682
Isac Barros <isac.brss@gmail.com>
2026-03-19 21:52:47 -0300

    Refactor Header component and update logo path

--- src/Components/portfolio/Header.jsx
+++ src/Components/portfolio/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Menu, X } from 'lucide-react';
-import logo from "./assets/logo.png";
-
+import logo from '.../../../logo.png';
 
 export default function Header({ scrollY }) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
