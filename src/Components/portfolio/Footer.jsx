0be3cd55f4f4bb1b98148420eef007c8cee5a323
Isac Barros <isac.brss@gmail.com>
2026-03-19 22:12:54 -0300

    Update Footer.jsx

--- src/Components/portfolio/Footer.jsx
+++ src/Components/portfolio/Footer.jsx
@@ -9,7 +9,8 @@ export default function Footer() {
           <div>
             <h3 className="text-2xl font-bold mb-4">Isac Barros</h3>
             <p className="text-white/70">
-              Evoluindo cada dia mais para chegar cada vez mais longe            </p>
+              Evoluindo cada dia mais para chegar cada vez mais longe
+            </p>
           </div>
 
           <div>
@@ -47,7 +48,8 @@ export default function Footer() {
             <h4 className="font-semibold mb-4">Contato</h4>
             <ul className="space-y-2 text-white/70">
               <li>isac.brs.contact@gmail.com</li>
-              <li>+55 77 99176-4296/li>
+              {/* O erro estava aqui embaixo: corrigido para </li> */}
+              <li>+55 77 99176-4296</li> 
               <li>Brasília, Brasil 📍</li>
             </ul>
           </div>
